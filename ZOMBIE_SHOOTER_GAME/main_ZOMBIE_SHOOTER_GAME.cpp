210c757887421539271dce1fee12cf7c380465bd
Exieter <65500222+Exieter@users.noreply.github.com>
2020-06-27 17:51:43 +0200

    SpawnZombieWCzasie
    Spawnuje zombie w czasie ( prawdopodobnie wymaga dopracowania)

--- ZOMBIE_SHOOTER_GAME/main_ZOMBIE_SHOOTER_GAME.cpp
+++ ZOMBIE_SHOOTER_GAME/main_ZOMBIE_SHOOTER_GAME.cpp
@@ -33,19 +33,68 @@ int main() {
 
 //obiekty
     Player player(texturePlayer);
-    SmallZombie smallzombie(textureZombie);
-    MediumZombie mediumzombie(textureZombie);
-    BigZombie bigzombie(textureZombie);
     Otoczenie otoczenie(textureSciana,texturePodloga);
 
 // sprite const
 
 std::vector<sf::Sprite> sciany_sprites = otoczenie.getScianyVectorSprite();
 
+//vector
+std::vector<std::unique_ptr<Zombie>> zombieVector;
+
+zombieVector.emplace_back(new SmallZombie(textureZombie));
+zombieVector.emplace_back(new MediumZombie(textureZombie));
+zombieVector.emplace_back(new BigZombie(textureZombie));
+//zmienne
+float time_to_spawn_smallzombie=0;
+float time_to_spawn_mediumzombie=0;
+float time_to_spawn_bigzombie=0;
+
+float time_enemies_mediumzombie=0;
+float time_enemies_smallzombie=0;
+float time_enemies_bigzombie=0;
 
     while (window.isOpen()) {
         sf::Event event;
-        sf::Clock clock;
+//spawn zombies:
+        //smallzombie
+        sf::Clock clock_to_spawn_smallzombie;
+
+         time_enemies_smallzombie = clock_to_spawn_smallzombie.getElapsedTime().asSeconds();
+        time_to_spawn_smallzombie = time_enemies_smallzombie + time_to_spawn_smallzombie * 1.0005;
+
+            if(time_to_spawn_smallzombie >= 0.001)
+            {
+            zombieVector.emplace_back(new SmallZombie(textureZombie));
+            time_to_spawn_smallzombie = 0;
+            }
+        //mediumzombie
+             sf::Clock clock_to_spawn_mediumzombie;
+
+              time_enemies_mediumzombie = clock_to_spawn_smallzombie.getElapsedTime().asSeconds();
+             time_to_spawn_mediumzombie = time_enemies_mediumzombie + time_to_spawn_mediumzombie * 1.0005;
+
+                 if(time_to_spawn_mediumzombie >= 0.05)
+                 {
+                 zombieVector.emplace_back(new MediumZombie(textureZombie));
+                 time_to_spawn_mediumzombie = 0;
+                 }
+         //bigzombie
+                 sf::Clock clock_to_spawn_bigzombie;
+
+                  time_enemies_bigzombie = clock_to_spawn_bigzombie.getElapsedTime().asSeconds();
+                 time_to_spawn_bigzombie = time_enemies_bigzombie + time_to_spawn_bigzombie * 1.0005;
+
+                     if(time_to_spawn_bigzombie >= 0.1)
+                     {
+                     zombieVector.emplace_back(new BigZombie(textureZombie));
+                     time_to_spawn_bigzombie = 0;
+                     }
+
+
+                     //
+
+     sf::Clock clock;
 
 //sprite variable
     sf::Sprite player_sprite = player.getSpritePlayer();
@@ -59,17 +108,21 @@ std::vector<sf::Sprite> sciany_sprites = otoczenie.getScianyVectorSprite();
 //poruszanie
         sf::Time elapsed = clock.restart();
         player.poruszanie(sciany_sprites,elapsed);
-        smallzombie.poruszanie(player_sprite,elapsed);
-        mediumzombie.poruszanie(player_sprite,elapsed);
-        bigzombie.poruszanie(player_sprite,elapsed);
+
+        for(auto &el : zombieVector)
+        {
+            el->poruszanie(player_sprite,elapsed);
+        }
 //rysowanie
         window.display();
 
         otoczenie.rysuj(window);
         player.rysuj(window);
-        smallzombie.rysuj(window);
-        mediumzombie.rysuj(window);
-        bigzombie.rysuj(window);
+
+        for(auto &el : zombieVector)
+        {
+            el->rysuj(window);
+        }
 
     }
 ;
